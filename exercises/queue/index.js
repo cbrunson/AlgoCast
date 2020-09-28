ee19015a03e8b7905be58c0768131b243c2a5685
Carl Brunson <carl.brunson@gmail.com>
2020-09-27 19:38:05 -0500

    master

--- exercises/queue/index.js
+++ exercises/queue/index.js
@@ -8,6 +8,45 @@
 //     q.add(1);
 //     q.remove(); // returns 1;
 
-class Queue {}
+const Stack = require('./stack');
+
+class Queue {
+    constructor() {
+        this.first = new Stack();
+        this.second = new Stack();
+    }
+
+    add(record) {
+        this.first.push(record);
+    }
+
+    remove() {
+        while (this.first.peek()) {
+            this.second.push(this.first.pop());
+        }
+
+        const record = this.first.pop();
+
+        while (this.second.peek()) {
+            this.first.push(this.second.pop());
+        }
+
+        return record;
+    }
+
+    peek() {
+        while (this.first.peek()) {
+            this.second.push(this.first.pop());
+        }
+
+        const record = this.second.peek();
+
+        while (this.second.peek()) {
+            this.first.push(ths.second.pop());
+        }
+
+        return record;
+    }
+}
 
 module.exports = Queue;
